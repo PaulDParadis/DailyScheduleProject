8271c29dd161afdd68cf8db34568a87ed2879dd0
Paul D. Paradis <52587549+PaulDParadis@users.noreply.github.com>
2019-09-04 22:29:34 -0400

    Create while_loop_timer.py

---
+++ while_loop_timer.py
@@ -0,0 +1,9 @@
+import time
+def TaskLength():
+    duration = int(input("Duration: ")
+    while duration > 0:
+        print(duration)
+        duration = -1
+TaskLength()
+print("Please record any ontes in the space provided.")
+print("Please press enter when finished to start time for next task.")
